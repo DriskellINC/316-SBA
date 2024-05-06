9e7db105d2df61cbdc9f4d63e60ed9bf3309b55a
Dionte Driskell <driskell06@gmail.com>
2024-05-06 07:27:50 -0400

    added id

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
-// Create an element requirement:
-const myItem = document.createElement("li");
+// // Create an element requirement:
+// const myItem = document.createElement("li");
 
-// Create a text:
-const textItem = document.createTextNode("footwear");
+// // Create a text:
+// const textItem = document.createTextNode("footwear");
 
-// Append the text node to the "li" node:
-myItem.appendChild(textItem);
+// // Append the text node to the "li" node:
+// myItem.appendChild(textItem);
 
-// Append the "li" node to the list:
-document.getElementById("myList").appendChild(myItem);
+// // Append the "li" node to the list:
+// document.getElementById("myList").appendChild(myItem);
 
 document.body.appendChild.(myItem);
 const fragment = document.createDocumentFragment();
 const li = fragment
-  .appendChild(document.createElement("section"))
-  .appendChild(document.createElement("ul"))
-  .appendChild(document.createElement("li"));
+    .appendChild(document.createElement("section"))
+    .appendChild(document.createElement("ul"))
+    .appendChild(document.createElement("li"));
 li.textContent = "footwear";
 
 document.body.appendChild(fragment);
+
+//add.eventListener requirements
 
