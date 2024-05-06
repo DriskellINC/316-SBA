528b43e1969c6573941521b908ef8e896c7aeb09
Dionte Driskell <driskell06@gmail.com>
2024-05-06 06:56:15 -0400

    firt commit

---
+++ script.js
@@ -0,0 +1,22 @@
+// Create an element requirement:
+const myItem = document.createElement("li");
+
+// Create a text:
+const textItem = document.createTextNode("footwear");
+
+// Append the text node to the "li" node:
+myItem.appendChild(textItem);
+
+// Append the "li" node to the list:
+document.getElementById("myList").appendChild(myItem);
+
+document.body.appendChild.(myItem);
+const fragment = document.createDocumentFragment();
+const li = fragment
+  .appendChild(document.createElement("section"))
+  .appendChild(document.createElement("ul"))
+  .appendChild(document.createElement("li"));
+li.textContent = "footwear";
+
+document.body.appendChild(fragment);
+
