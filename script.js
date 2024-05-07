129f98d08ada55580333c0e923ca59461136b3fa
Dionte Driskell <driskell06@gmail.com>
2024-05-06 21:09:45 -0400

    working code

--- script.js
+++ script.js
@@ -1,24 +1,44 @@
-// // Create an element requirement:
-// const myItem = document.createElement("li");
+ // Cache the ul element with id myList
+ const myList = document.getElementById("myList");
 
-// // Create a text:
-// const textItem = document.createTextNode("footwear");
+ // Create a new li element
+ const newItem = document.createElement("li");
 
-// // Append the text node to the "li" node:
-// myItem.appendChild(textItem);
+ // Create a text node
+ const textNode = document.createTextNode("suit");
 
-// // Append the "li" node to the list:
-// document.getElementById("myList").appendChild(myItem);
+ // Append the text node to the li element
+ newItem.appendChild(textNode);
 
-document.body.appendChild.(myItem);
-const fragment = document.createDocumentFragment();
-const li = fragment
-    .appendChild(document.createElement("section"))
-    .appendChild(document.createElement("ul"))
-    .appendChild(document.createElement("li"));
-li.textContent = "footwear";
+ // Append the li node to the ul list
+ myList.appendChild(newItem);
 
-document.body.appendChild(fragment);
+// document.body.appendChild(myItem);
 
-//add.eventListener requirements
 
+// document.body.appendChild.(myItem);
+// const fragment = document.createDocumentFragment();
+// const li = fragment
+//     .appendChild(document.createElement("section"))
+//     .appendChild(document.createElement("ul"))
+//     .appendChild(document.createElement("li"));
+// li.textContent = "belts";
+
+// document.body.appendChild(fragment);
+
+// //add.eventListener requirements
+// const flex = document.getElementById("flex");
+// .addEventListener('keypress', checkIfEnter);
+// .addEventListener('keypress', checkIfEnter);
+
+// function checkIfEnter(event){
+//   if (event.keyCode === 13) {
+//     getResults("flex".value);
+//   }
+// }
+
+// flex.addEventListener('click', userClicked);
+
+// function userClicked(event){
+//     getResults(flex.value);
+}
